27f31c915187bd67e78b6fcfdf950012ab218d18
alumno1 alumno1 <alumno1@alumno1.com>
2025-12-11 01:04:17 +0000

    Edit MeController.java

--- src/main/java/com/upiiz/platform_api/controller/MeController.java
+++ src/main/java/com/upiiz/platform_api/controller/MeController.java
@@ -25,7 +25,7 @@ import java.nio.file.StandardCopyOption;
 
 import java.util.*;
 
-@CrossOrigin(origins = "http://148.204.142.20:4200")
+@CrossOrigin(origins = "http://148.204.142.20:3030")
 @RestController
 @RequestMapping("/upiiz/public/v1/me")
 @Tag(name = "Perfil", description = "Operaciones relacionadas con el perfil del usuario autenticado")
